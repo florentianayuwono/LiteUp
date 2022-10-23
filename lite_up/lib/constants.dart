f50ef07032a1d4df808da77dc31f2273df4c2eb1
florentianayuwono <e0851439@u.nus.edu>
2022-10-23 15:36:42 +0800

    Set up color theme

---
+++ lite_up/lib/constants.dart
@@ -0,0 +1,8 @@
+// This file contains customization for colors and fonts.
+import 'package:flutter/material.dart';
+
+const Color background = Color.fromARGB(255, 249, 240, 181);
+const Color deepOrange = Color.fromARGB(255, 255, 123, 46);
+const Color softOrange = Color.fromARGB(255, 254, 189, 142);
+const Color deepYellow = Color.fromARGB(255, 255, 187, 1);
+const Color skyBlue = Color.fromARGB(255, 156, 197, 231);
